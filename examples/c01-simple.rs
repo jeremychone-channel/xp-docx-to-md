2857a5d075c23119ee223a31db50aa8f58008718
Jeremy Chone <jeremy.chone@gmail.com>
2025-08-11 12:56:24 -0700

    . c01-simple - with markdownify

--- examples/c01-simple.rs
+++ examples/c01-simple.rs
@@ -1,5 +1,18 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello World");
+use markdownify::docx;
+use std::path::Path;
 
-    Ok(())
+const DOC_PATH: &str = ".data/doc-simple-nda.docx";
+const MD_PATH: &str = ".data/c01-simple.md";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+	// Convert a Word document
+	let path = Path::new(DOC_PATH);
+	let markdown = docx::docx_convert(path)?;
+
+	// save mardown to MD_PATH
+	std::fs::write(MD_PATH, &markdown)?;
+
+	println!("Save to {MD_PATH}");
+
+	Ok(())
 }
